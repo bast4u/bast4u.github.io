538f72160c314189891654ebb1d74b08bc3afdb0
bast4u <132562576+bast4u@users.noreply.github.com>
2023-06-23 16:50:44 +0200

    .

---
+++ portfolio/contact.php
@@ -0,0 +1,24 @@
+
+<?php
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   $name = $_POST['name'];
+//   $email = $_POST['email'];
+//   $message = $_POST['message'];
+
+//   $to = "bastien-stachs@hotmail.fr"; // Remplacez cette adresse e-mail par votre propre adresse
+
+//   $subject = "Nouveau message du formulaire de contact";
+//   $body = "Nom: $name\n\n";
+//   $body .= "Email: $email\n\n";
+//   $body .= "Message:\n$message\n";
+
+//   $headers = "From: $email\r\n";
+//   $headers .= "Reply-To: $email\r\n";
+
+//   if (mail($to, $subject, $body, $headers)) {
+//     echo "Votre message a été envoyé avec succès.";
+//   } else {
+//     echo "Une erreur s'est produite lors de l'envoi du message.";
+//   }
+// }
+?>
